48cd722f3dd39315f9424f67bb5b393ffdd01216
maximemlly <maxime.maillary@icloud.com>
2026-07-02 16:29:42 +0200

    fix: update contact email in Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -40,7 +40,7 @@ export default function Footer() {
                     <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
                   </svg>
                 </span>
-                contact@maxime-maillary.fr
+                maxime.maillary@coda-student.school
               </a>
               <a
                 href="tel:+33623017880"
